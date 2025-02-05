7c61d4652571e9ba5c88c6974061d32e36eb31d3
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-02-05 00:24:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,47 +6,6 @@
 
 import { getUserIds } from "./storage.js";
 
-function displayAgenda(userId){
- const agendaContainer = document.getElementById("agenda") 
- agendaContainer.textContent = ""
-  const agenda = getData(userId)
-  if (!agenda || agenda.length === 0) {
-    document.getElementById('agenda').innerHTML = 'No agenda available for this user.';
-    return;
-  }
-  const today = new Date();
-  const futureAgenda = agenda.filter(item => new Date(item.date) >= today);
-
-  futureAgenda.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-  let agendaHtml = `
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Topic</th>
-            <th>Revision Date</th>
-          </tr>
-        </thead>
-        <tbody>
-    `;
-
-    futureAgenda.forEach(item => {
-      const topic = item.topic;
-      const date = new Date(item.date).toLocaleDateString();
-      agendaHtml += `
-        <tr>
-          <td>${topic}</td>
-          <td>${date}</td>
-        </tr>
-      `;
-    });
-
-  agendaHtml += '</tbody></table>';
-
-  document.getElementById('agenda').innerHTML = agendaHtml;
-}
-
-
 window.onload = function () {
   populateUserDropdown();
 
@@ -62,7 +21,7 @@ function populateUserDropdown() {
 
   document.querySelector("p").innerText = `There are ${users.length} users`;
 };
-=======
+
 
   // Create a default option
   const defaultOption = document.createElement("option");
@@ -77,7 +36,7 @@ function populateUserDropdown() {
     option.textContent = `User ${userId}`;
     userSelect.appendChild(option);
   });
-}
+
 
 // Handles user selection and displays a static message
 function handleUserSelection(event) {
@@ -95,5 +54,4 @@ function handleUserSelection(event) {
   }
 }
 
-feature/display-agenda
-main
+
